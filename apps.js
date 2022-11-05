23af331347a7932175dd8f05375a7b449ed09d20
Adam <adamlederer@live.ca>
2022-11-05 13:35:47 -0600

    finished knowledge check

---
+++ apps.js
@@ -0,0 +1,13 @@
+let company ={
+    companyName: `DasNice`,
+    yearRevenue: 12000000,
+    ceo: `Adam`,
+    numberEmployees: 50
+}
+
+if (company[`yearRevenue`] >= 1000000) {
+    console.log("Congratulations", company["companyName"], 
+    company[`ceo`], company[`numberEmployees`]);    
+} else{
+    console.log(`You will learn to keep growing.`);
+}
