a2959e953fc4a2168895812a5198bde969b9afae
Adam <adamlederer@live.ca>
2022-11-05 13:54:26 -0600

    added comments for another way to console.log.

--- apps.js
+++ apps.js
@@ -1,12 +1,16 @@
 let company ={
-    Name: `DasNice`,
+    name: `DasNice`,
     yearRevenue: 12000000,
     ceo: `Adam`,
     numberEmployees: 50
 }
 
 if (company[`yearRevenue`] >= 1000000) {
-    console.log("Congratulations", company["companyName"], 
+    //console.log(`Congratulations on being a company with money`)
+    //console.log(company[`name`])
+    //console.log(company[`ceo`])
+    //console.log(company[`numberEmployees`])
+    console.log("Congratulations", company["name"], 
     company[`ceo`], company[`numberEmployees`]);    
 } else{
     console.log(`You will learn to keep growing.`);
